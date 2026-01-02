2e919f3ad7a42f52f2fbec2ac998784332ce693a
travis@travestyofcode.com <travis@travestyofcode.com>
2026-01-02 00:20:45 -0500

    Adds additional tests for Lot and Serial numbers.

--- QB.Tests/InventoryAdjustmentAddRqTests.cs
+++ QB.Tests/InventoryAdjustmentAddRqTests.cs
@@ -17,7 +17,6 @@ public class InventoryAdjustmentAddRqTests(QBXMLSchemaFixture fixture) : IClassF
             InventoryAdjustmentLineAdd =
             [
                 new QuantityAdjustment(){ ItemRef = "A", NewQuantity  = 10 },
-                new ValueAdjustment(){ItemRef = "B", NewValue = 100m }
             ]
 
         };
@@ -47,6 +46,65 @@ public class InventoryAdjustmentAddRqTests(QBXMLSchemaFixture fixture) : IClassF
             InventoryAdjustmentLineAdd =
             [
                 new ValueAdjustment() { ItemRef = "B", NewValue = 100m }
+            ]
+
+        };
+
+        var rq = new QBXMLRequest([addRq]);
+
+        // Act
+        string validationErrors = string.Empty;
+        rq.ToXDocument().Validate(fixture.QBXMLSchema, (o, e) =>
+        {
+            validationErrors += e.Message + Environment.NewLine;
+        });
+
+        // Assert
+        Assert.Equal<object>(string.Empty, validationErrors);
+    }
+
+    [Fact]
+    public void LotNumberAdjustmentGeneratesCorrectRequestString()
+    {
+        // Arrange
+        var addRq = new InventoryAdjustmentAdd()
+        {
+            AccountRef = "COGS",
+            TxnDate = new DateOnly(2025, 12, 31),
+            Memo = "Adjustment",
+            InventoryAdjustmentLineAdd =
+            [
+                new LotNumberAdjustment(){ ItemRef = "A", LotNumber = "123", CountAdjustment = 0, InventorySiteLocationRef = "Location"}
+            ]
+
+        };
+
+        var rq = new QBXMLRequest([addRq]);
+
+        // Act
+        string validationErrors = string.Empty;
+        rq.ToXDocument().Validate(fixture.QBXMLSchema, (o, e) =>
+        {
+            validationErrors += e.Message + Environment.NewLine;
+        });
+
+        // Assert
+        Assert.Equal<object>(string.Empty, validationErrors);
+    }
+
+    [Fact]
+    public void SerialNumberAdjustmentGeneratesCorrectRequestString()
+    {
+        // Arrange
+        var addRq = new InventoryAdjustmentAdd()
+        {
+            AccountRef = "COGS",
+            TxnDate = new DateOnly(2025, 12, 31),
+            Memo = "Adjustment",
+            InventoryAdjustmentLineAdd =
+            [
+                new SerialNumberAdjustment(){ ItemRef = "A", AddSerialNumber = "ABC123", InventorySiteLocationRef = "Location" },
+                new SerialNumberAdjustment(){ ItemRef = "A", RemoveSerialNumber = "ABC123", InventorySiteLocationRef = "Location" },
             ]
 
         };
