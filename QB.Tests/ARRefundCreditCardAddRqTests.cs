d14e879986608fb4beaa8528495699c39813eb5a
travis@travestyofcode.com <travis@travestyofcode.com>
2026-01-02 00:33:54 -0500

    Adds tests for ARRefundCreditCardAddRq.

---
+++ QB.Tests/ARRefundCreditCardAddRqTests.cs
@@ -0,0 +1,33 @@
+﻿using QB.SDK;
+
+namespace QB.Tests;
+
+public class ARRefundCreditCardAddRqTests(QBXMLSchemaFixture fixture) : IClassFixture<QBXMLSchemaFixture>
+{
+    [Fact]
+    public void GeneratesCorrectRequestString()
+    {
+        // Arrange
+        var addRq = new ARRefundCreditCardAdd()
+        {
+            ARAccountRef = "Accounts Receivable",
+            TxnDate = new DateOnly(2025, 12, 31),
+            RefundAppliedToTxnAdd =
+            [
+                new RefundAppliedToTxnAdd(){ RefundAmount = 100m , TxnID = "123abc" }
+            ]
+        };
+
+        var rq = new QBXMLRequest([addRq]);
+
+        // Act
+        string validationErrors = string.Empty;
+        rq.ToXDocument().Validate(fixture.QBXMLSchema, (o, e) =>
+        {
+            validationErrors += e.Message + Environment.NewLine;
+        });
+
+        // Assert
+        Assert.Equal<object>(string.Empty, validationErrors);
+    }
+}
