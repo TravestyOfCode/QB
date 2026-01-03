f99dbe0dc5d5a602a20dcf02ee8e7d6576e04b4b
travis@travestyofcode.com <travis@travestyofcode.com>
2026-01-03 11:09:07 -0500

    Adds CustomerAdd tests.

---
+++ QB.Tests/Customers/CustomerAddRqTests.cs
@@ -0,0 +1,35 @@
+﻿using QB.SDK;
+
+namespace QB.Tests.Customers;
+
+public class CustomerAddRqTests(QBXMLSchemaFixture fixture) : IClassFixture<QBXMLSchemaFixture>
+{
+    [Fact]
+    public void GeneratesCorrectRequestString()
+    {
+        // Arrange
+        var rq = new CustomerAdd()
+        {
+            Name = "Customer",
+            ParentRef = "Parent",
+            AdditionalNotes = [
+                new() { Note = "TestNote" }
+                ],
+            Contacts = [
+                new() { FirstName = "ContactFirst", AdditionalContactRef = [ AdditionalContact.Email("bob@bob.com")]}
+                ]
+        };
+
+        var qbxml = new QBXMLRequest([rq]);
+
+        // Act
+        string validationErrors = string.Empty;
+        qbxml.ToXDocument().Validate(fixture.QBXMLSchema, (o, e) =>
+        {
+            validationErrors += e.Message + Environment.NewLine;
+        });
+
+        // Assert
+        Assert.Equal<object>(string.Empty, validationErrors);
+    }
+}
