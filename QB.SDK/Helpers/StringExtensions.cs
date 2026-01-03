700d2543f543d4a70606556c2e314e1a0b9780f9
travis@travestyofcode.com <travis@travestyofcode.com>
2026-01-03 08:33:20 -0500

    Adds string extensions for null checks.

---
+++ QB.SDK/Helpers/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace QB.SDK;
+
+public static class StringExtensions
+{
+    public static bool IsNullOrWhiteSpace([NotNullWhen(false)] this string? value) => string.IsNullOrWhiteSpace(value);
+    public static bool IsNotNullOrWhiteSpace([NotNullWhen(true)] this string? value) => !string.IsNullOrWhiteSpace(value);
+    public static void ThrowIfNullOrWhiteSpace([NotNull] this string? value, [CallerArgumentExpression(nameof(value))] string name = "")
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new ArgumentException($"{name} can not be null or empty.");
+        }
+    }
+}
