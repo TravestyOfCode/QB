cf369b57cb2a61d2994e7b5ff6313777f6d1161d
travis@travestyofcode.com <travis@travestyofcode.com>
2026-01-03 07:18:45 -0500

    Adds test for SalesOrderQuery.

---
+++ QB.Tests/SalesOrder/SalesOrderQueryRqTests.cs
@@ -0,0 +1,28 @@
+﻿using QB.SDK;
+
+namespace QB.Tests.SalesOrder;
+
+public class SalesOrderQueryRqTests(QBXMLSchemaFixture fixture) : IClassFixture<QBXMLSchemaFixture>
+{
+    [Fact]
+    public void GeneratesCorrectRequestString()
+    {
+        // Arrange
+        var rq = new SalesOrderQuery()
+        {
+            RefNumber = ["12345"],
+        };
+
+        var qbxml = new QBXMLRequest([rq]);
+
+        // Act
+        string validationErrors = string.Empty;
+        qbxml.ToXDocument().Validate(fixture.QBXMLSchema, (o, e) =>
+        {
+            validationErrors += e.Message + Environment.NewLine;
+        });
+
+        // Assert
+        Assert.Equal<object>(string.Empty, validationErrors);
+    }
+}
