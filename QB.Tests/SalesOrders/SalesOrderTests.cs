bdc017a6791723287bc014f15e3b5a5773a13258
travis@travestyofcode.com <travis@travestyofcode.com>
2026-01-03 09:03:52 -0500

    Adds ToMod tests.

---
+++ QB.Tests/SalesOrders/SalesOrderTests.cs
@@ -0,0 +1,73 @@
+﻿using QB.SDK;
+
+namespace QB.Tests.SalesOrders;
+
+public class SalesOrderTests(QBXMLSchemaFixture fixture) : IClassFixture<QBXMLSchemaFixture>
+{
+    [Fact]
+    public void ToModThrowsArgumentExceptionOnNullTxnID()
+    {
+        var so = new SalesOrder();
+
+        Assert.Throws<ArgumentException>(() => so.ToMod());
+    }
+
+    [Fact]
+    public void ToModThrowsArgumentExceptionOnEmptyTxnID()
+    {
+        var so = new SalesOrder() { TxnID = string.Empty };
+
+        Assert.Throws<ArgumentException>(() => so.ToMod());
+    }
+
+    [Fact]
+    public void ToModThrowsArgumentExceptionOnNullEditSequence()
+    {
+        var so = new SalesOrder();
+
+        Assert.Throws<ArgumentException>(() => so.ToMod());
+    }
+
+    [Fact]
+    public void ToModThrowsArgumentExceptionOnEmptyEditSequence()
+    {
+        var so = new SalesOrder() { EditSequence = string.Empty };
+
+        Assert.Throws<ArgumentException>(() => so.ToMod());
+    }
+
+    [Fact]
+    public void ToModGeneratesCorrectRequestString()
+    {
+        // Arrange
+        var so = new SalesOrder()
+        {
+            TxnID = "ABC123",
+            EditSequence = "AAAA-BBBBB",
+            CustomerRef = "Customer",
+            TxnDate = new DateOnly(2025, 12, 31),
+            SalesOrderLines =
+            [
+                new SalesOrderLine(){ TxnLineID = "AAA", ItemRef = "OH", Desc = "This is a description", Amount = 100m },
+                new SalesOrderLineGroup(){ TxnLineID = "BBB", ItemGroupRef = "A", SalesOrderLineRet =
+                [
+                    new SalesOrderLine(){ TxnLineID = "AAA", ItemRef = "OH", Desc = "This is a description", Amount = 100m },
+                    new SalesOrderLine(){ TxnLineID = "AAA", ItemRef = "OH", Desc = "This is a description", Amount = 100m },
+                    new SalesOrderLine(){ TxnLineID = "AAA", ItemRef = "OH", Desc = "This is a description", Amount = 100m }
+                ]}
+            ]
+        };
+
+        var qbxml = new QBXMLRequest([so.ToMod()]);
+
+        // Act
+        string validationErrors = string.Empty;
+        qbxml.ToXDocument().Validate(fixture.QBXMLSchema, (o, e) =>
+        {
+            validationErrors += e.Message + Environment.NewLine;
+        });
+
+        // Assert
+        Assert.Equal<object>(string.Empty, validationErrors);
+    }
+}
