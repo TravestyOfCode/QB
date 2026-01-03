939221bacbca9f8d6db351aca8e422decf0ddf6b
travis@travestyofcode.com <travis@travestyofcode.com>
2026-01-03 07:12:45 -0500

    Adds SalesOrderMod tests.

---
+++ QB.Tests/SalesOrder/SalesOrderModRqTests.cs
@@ -0,0 +1,35 @@
+﻿using QB.SDK;
+
+namespace QB.Tests.SalesOrder;
+
+public class SalesOrderModRqTests(QBXMLSchemaFixture fixture) : IClassFixture<QBXMLSchemaFixture>
+{
+    [Fact]
+    public void GeneratesCorrectRequestString()
+    {
+        // Arrange
+        var rq = new SalesOrderMod()
+        {
+            CustomerRef = "TestCustomer",
+            TxnDate = new DateOnly(2025, 12, 27),
+            RefNumber = "12345",
+            SalesOrderLineMod =
+            [
+                new SalesOrderLineMod(){ ItemRef = "Item", Amount = 100.0m },
+                new SalesOrderLineGroupMod() { ItemGroupRef = "A" }
+            ]
+        };
+
+        var qbxml = new QBXMLRequest([rq]);
+
+        // Act
+        string validationErrors = string.Empty;
+        qbxml.ToXDocument().Validate(fixture.QBXMLSchema, (o, e) =>
+        {
+            validationErrors += e.Message + Environment.NewLine;
+        });
+
+        // Assert
+        Assert.Equal<object>(string.Empty, validationErrors);
+    }
+}
