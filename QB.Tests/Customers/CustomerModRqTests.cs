6e7d5b3503d41a4db10722904884eda966ebd236
travis@travestyofcode.com <travis@travestyofcode.com>
2026-01-03 11:34:56 -0500

    Adds tests for Customers.

---
+++ QB.Tests/Customers/CustomerModRqTests.cs
@@ -0,0 +1,30 @@
+﻿using QB.SDK;
+
+namespace QB.Tests.Customers;
+
+public class CustomerModRqTests(QBXMLSchemaFixture fixture) : IClassFixture<QBXMLSchemaFixture>
+{
+    [Fact]
+    public void GeneratesCorrectRequestString()
+    {
+        // Arrange
+        var rq = new CustomerMod()
+        {
+            ListID = "ABC123",
+            EditSequence = "123ABC",
+            Name = "CustomerName"
+        };
+
+        var qbxml = new QBXMLRequest([rq]);
+
+        // Act
+        string validationErrors = string.Empty;
+        qbxml.ToXDocument().Validate(fixture.QBXMLSchema, (o, e) =>
+        {
+            validationErrors += e.Message + Environment.NewLine;
+        });
+
+        // Assert
+        Assert.Equal<object>(string.Empty, validationErrors);
+    }
+}
---
+++ QB.Tests/Customers/CustomerTests.cs
@@ -0,0 +1,66 @@
+﻿using QB.SDK;
+
+namespace QB.Tests.Customers;
+
+public class CustomerTests(QBXMLSchemaFixture fixture) : IClassFixture<QBXMLSchemaFixture>
+{
+    [Fact]
+    public void ToModThrowsArgumentExceptionOnNullListID()
+    {
+        var so = new Customer();
+
+        Assert.Throws<ArgumentException>(() => so.ToMod());
+    }
+
+    [Fact]
+    public void ToModThrowsArgumentExceptionOnEmptyListID()
+    {
+        var so = new Customer() { ListID = string.Empty };
+
+        Assert.Throws<ArgumentException>(() => so.ToMod());
+    }
+
+    [Fact]
+    public void ToModThrowsArgumentExceptionOnNullEditSequence()
+    {
+        var so = new Customer();
+
+        Assert.Throws<ArgumentException>(() => so.ToMod());
+    }
+
+    [Fact]
+    public void ToModThrowsArgumentExceptionOnEmptyEditSequence()
+    {
+        var so = new Customer() { EditSequence = string.Empty };
+
+        Assert.Throws<ArgumentException>(() => so.ToMod());
+    }
+
+    [Fact]
+    public void ToModGeneratesCorrectRequestString()
+    {
+        // Arrange
+        var so = new Customer()
+        {
+            ListID = "ABC123",
+            EditSequence = "AAAA-BBBBB",
+            Name = "Customer",
+            AdditionalNotesRet = [
+                new AdditionalNote(){ Note = "Note", NoteID = 123 },
+                new AdditionalNote(){ Note = "Note", NoteID = 234 }
+                ]
+        };
+
+        var qbxml = new QBXMLRequest([so.ToMod()]);
+
+        // Act
+        string validationErrors = string.Empty;
+        qbxml.ToXDocument().Validate(fixture.QBXMLSchema, (o, e) =>
+        {
+            validationErrors += e.Message + Environment.NewLine;
+        });
+
+        // Assert
+        Assert.Equal<object>(string.Empty, validationErrors);
+    }
+}
