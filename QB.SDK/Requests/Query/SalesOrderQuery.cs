5bfbaf667881d2be0464b56755c9ae4675ba4083
travis@travestyofcode.com <travis@travestyofcode.com>
2026-01-04 16:09:06 -0500

    Migrated query to use TxnNoAccount  base. Updated tests for exclusive properties and generting correct xml.

--- QB.SDK/Requests/Query/SalesOrderQuery.cs
+++ QB.SDK/Requests/Query/SalesOrderQuery.cs
@@ -2,21 +2,12 @@
 
 namespace QB.SDK;
 
-public class SalesOrderQuery : QueryRq, IQBXML
+public class SalesOrderQuery : TxnNoAccountQuery
 {
-    public List<string>? TxnID { get; set; }
-    public List<string>? RefNumber { get; set; }
-    public List<string>? RefNumberCaseSensitive { get; set; }
-    public DateTimeRangeFilter? ModifiedDateRangeFilter { get; set; }
-    public DateRangeFilter? TxnDateRangeFilter { get; set; }
-    public ParentListFilter? EntityFilter { get; set; }
-    public StringFilter? RefNumberFilter { get; set; }
-    public StringRangeFilter? RefNumberRangeFilter { get; set; }
-    public ListFilter? CurrencyFilter { get; set; }
-    public bool? IncludeLineItems { get; set; }
-    public bool? IncludeLinkedTxns { get; set; }
-    public List<string>? OwnerID { get; set; }
-
+    /// <summary>
+    /// Converts the object to a XElement represenation according to the QBXML specification.
+    /// </summary>
+    /// <returns>A XElement respresentation of the object.</returns>
     public override XElement ToQBXML()
     {
         return new XElement($"{nameof(SalesOrderQuery)}Rq")
@@ -40,21 +31,43 @@ public class SalesOrderQuery : QueryRq, IQBXML
             .Append(OwnerID);
     }
 
+    /// <summary>
+    /// Generates a SalesOrderQuery request filtering by a list of RefNumbers.
+    /// </summary>
+    /// <param name="refNumbers">The list of RefNumbers to use for filtering.</param>
+    /// <param name="isCaseSensitive">Determines if the filter list is case sensitive. Recommend to leave as true (default) if case sensitive does not matter.</param>
+    /// <returns>A SalesOrderQuery request.</returns>
     public static SalesOrderQuery ByRefNumbers(IEnumerable<string> refNumbers, bool isCaseSensitive = true)
     {
         return isCaseSensitive ? new() { RefNumberCaseSensitive = refNumbers.ToList() } : new() { RefNumber = refNumbers.ToList() };
     }
 
+    /// <summary>
+    /// Generates a SalesOrderQuery request filtering by a single RefNumber.
+    /// </summary>
+    /// <param name="refNumbers">The RefNumber to use for filtering.</param>
+    /// <param name="isCaseSensitive">Determines if the RefNumber is case sensitive. Recommend to leave as true (default) if case sensitive does not matter.</param>
+    /// <returns>A SalesOrderQuery request.</returns>
     public static SalesOrderQuery ByRefNumber(string refNumber, bool isCaseSensitive = true)
     {
         return isCaseSensitive ? new() { RefNumberCaseSensitive = [refNumber] } : new() { RefNumber = [refNumber] };
     }
 
+    /// <summary>
+    /// Generates a SalesOrderQuery request filtering by a list of TxnIDs.
+    /// </summary>
+    /// <param name="refNumbers">The list of TxnIDs to use for filtering.</param>
+    /// <returns>A SalesOrderQuery request.</returns>
     public static SalesOrderQuery ByTxnIDs(IEnumerable<string> txnIDs)
     {
         return new() { TxnID = [.. txnIDs] };
     }
 
+    /// <summary>
+    /// Generates a SalesOrderQuery request filtering by a specific TxnIDs.
+    /// </summary>
+    /// <param name="refNumbers">A TxnIDs to use for filtering.</param>
+    /// <returns>A SalesOrderQuery request.</returns>
     public static SalesOrderQuery ByTxnID(string txnID)
     {
         return new() { TxnID = [txnID] };
@@ -63,24 +76,69 @@ public class SalesOrderQuery : QueryRq, IQBXML
 
 public static class SalesOrderQueryExtensions
 {
-    public static QBXMLResponse FindSalesOrdersByRefNumber(this QBConnection qbConnection, string refNumber, bool isCaseSensitive = true)
+    /// <summary>
+    /// Queries QuickBooks for a single SalesOrder with a specific RefNumber.
+    /// </summary>
+    /// <param name="qbConnection">The QB Connection to execute the query.</param>
+    /// <param name="refNumber">The specific RefNumber to query for.</param>
+    /// <param name="isCaseSensitive">Determines if the RefNumber is case sensitive. Recommend to leave as true (default) if case sensitive does not matter.</param>
+    /// <returns>The SalesOrder with the requested RefNumber if it was found, otherwise null.</returns>
+    public static SalesOrder? FindSalesOrdersByRefNumber(this QBConnection qbConnection, string refNumber, bool isCaseSensitive = true)
     {
         // Generate the request using the static constructor
-        var rq = new QBXMLRequest([SalesOrderQuery.ByRefNumber(refNumber, isCaseSensitive)]);
+        var request = new QBXMLRequest([SalesOrderQuery.ByRefNumber(refNumber, isCaseSensitive)]);
 
-        // Process the request and send the result.
-        return qbConnection.ProcessRequest(rq);
+        // Process the request.
+        var response = qbConnection.ProcessRequest(request);
+
+        // Check if we have a successful response.
+        var soResponse = response.QBXMLMsgsRs?.Results?[0] as SalesOrderQueryRs ?? throw new QBSDKException();
+
+        // Found result: "0", Not Found result: "500"
+        if (soResponse.StatusCode == "0" || soResponse.StatusCode == "500")
+        {
+            return soResponse.Results?[0];
+        }
+
+        // Some other error occured.
+        throw new QBSDKException(soResponse);
     }
 
-    public static QBXMLResponse FindSalesOrdersByRefNumbers(this QBConnection qbConnection, IEnumerable<string> refNumbers, bool isCaseSensitive = true)
+    /// <summary>
+    /// Queries QuickBooks for a single SalesOrder with a specific RefNumber.
+    /// </summary>
+    /// <param name="qbConnection">The QB Connection to execute the query.</param>
+    /// <param name="refNumbers">The list of RefNumbers to query for.</param>
+    /// <param name="isCaseSensitive">Determines if the RefNumbers are case sensitive. Recommend to leave as true (default) if case sensitive does not matter.</param>
+    /// <returns>A List<SalesOrder> with the requested RefNumbers if they were found, otherwise null.</returns>
+    public static List<SalesOrder>? FindSalesOrdersByRefNumbers(this QBConnection qbConnection, IEnumerable<string> refNumbers, bool isCaseSensitive = true)
     {
         // Generate the request using the static constructor
-        var rq = new QBXMLRequest([SalesOrderQuery.ByRefNumbers(refNumbers, isCaseSensitive)]);
+        var request = new QBXMLRequest([SalesOrderQuery.ByRefNumbers(refNumbers, isCaseSensitive)]);
 
-        // Process the request and send the result.
-        return qbConnection.ProcessRequest(rq);
+        // Process the request.
+        var response = qbConnection.ProcessRequest(request);
+
+        // Check if we have a successful response.
+        var soResponse = response.QBXMLMsgsRs?.Results?[0] as SalesOrderQueryRs ?? throw new QBSDKException();
+
+        // Found result: "0", Not Found result: "500"
+        if (soResponse.StatusCode == "0" || soResponse.StatusCode == "500")
+        {
+            return soResponse.Results;
+        }
+
+        // Some other error occured.
+        throw new QBSDKException(soResponse);
     }
 
+    /// <summary>
+    /// Queries QuickBooks for a single SalesOrder with a specific TxnID.
+    /// </summary>
+    /// <param name="qbConnection">The QB Connection to execute the query.</param>
+    /// <param name="txnID">The specific TxnID to query for.</param>
+    /// <returns>The SalesOrder with the requested TxnID. Throws <see cref="QBSDKException">QBSDKException</see> if the SalesOrder does not exist.</returns>
+    /// <exception cref="QBSDKException">Thrown when there was an error processing the query, including if the requested TxnID does not exist.</exception>
     public static SalesOrder GetSalesOrderByTxnID(this QBConnection qbConnection, string txnID)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(nameof(txnID));
@@ -92,22 +150,25 @@ public static class SalesOrderQueryExtensions
         var response = qbConnection.ProcessRequest(request);
 
         // Check if we have a successful response.
-        var soResponse = response.QBXMLMsgsRs?.Results?[0] as SalesOrderQueryRs;
-        if (soResponse == null || soResponse.StatusCode != "0")
+        var soResponse = response.QBXMLMsgsRs?.Results?[0] as SalesOrderQueryRs ?? throw new QBSDKException();
+
+        // Found result: "0"
+        if (soResponse.StatusCode == "0")
         {
-            throw new KeyNotFoundException($"A SalesOrder with TxnID '{txnID}' was not found. The error was: {soResponse?.StatusMessage}");
+            return soResponse.Results![0];
         }
 
-        // Return the SalesOrder if we have only one, otherwise throw an exception.
-        return soResponse.Results?.Count switch
-        {
-            null => throw new InvalidOperationException("The SalesOrder response list was null."),
-            0 => throw new InvalidOperationException("The SalesOrder response list was empty."),
-            1 => soResponse.Results[0],
-            _ => throw new InvalidOperationException($"The SalesOrder response list contains more than one SalesOrder with TxnID '{txnID}'.")
-        };
+        // Some error occured, including Not Found "500"
+        throw new QBSDKException(soResponse);
     }
 
+    /// <summary>
+    /// Queries QuickBooks for a single SalesOrder with a specific RefNumber.
+    /// </summary>
+    /// <param name="qbConnection">The QB Connection to execute the query.</param>
+    /// <param name="refNumber">The specific TxnID to query for.</param>
+    /// <returns>The SalesOrder with the requested RefNumber. Throws <see cref="QBSDKException">QBSDKException</see> if the SalesOrder does not exist.</returns>
+    /// <exception cref="QBSDKException">Thrown when there was an error processing the query, including if the requested RefNumber does not exist.</exception>
     public static SalesOrder GetSalesOrderByRefNumber(this QBConnection qbConnection, string refNumber)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(nameof(refNumber));
@@ -119,19 +180,15 @@ public static class SalesOrderQueryExtensions
         var response = qbConnection.ProcessRequest(request);
 
         // Check if we have a successful response.
-        var soResponse = response.QBXMLMsgsRs?.Results?[0] as SalesOrderQueryRs;
-        if (soResponse == null || soResponse.StatusCode != "0")
+        var soResponse = response.QBXMLMsgsRs?.Results?[0] as SalesOrderQueryRs ?? throw new QBSDKException();
+
+        // Found result: "0"
+        if (soResponse.StatusCode == "0")
         {
-            throw new KeyNotFoundException($"A SalesOrder with RefNumber '{refNumber}' was not found. The error was: {soResponse?.StatusMessage}");
+            return soResponse.Results![0];
         }
 
-        // Return the SalesOrder if we have only one, otherwise throw an exception.
-        return soResponse.Results?.Count switch
-        {
-            null => throw new InvalidOperationException("The SalesOrder response list was null."),
-            0 => throw new InvalidOperationException("The SalesOrder response list was empty."),
-            1 => soResponse.Results[0],
-            _ => throw new InvalidOperationException($"The SalesOrder response list contains more than one SalesOrder with RefNumber '{refNumber}'.")
-        };
+        // Some error occured, including Not Found "500"
+        throw new QBSDKException(soResponse);
     }
 }
--- QB.Tests/SalesOrders/SalesOrderQueryRqTests.cs
+++ QB.Tests/SalesOrders/SalesOrderQueryRqTests.cs
@@ -5,12 +5,146 @@ namespace QB.Tests.SalesOrders;
 public class SalesOrderQueryRqTests(QBXMLSchemaFixture fixture) : IClassFixture<QBXMLSchemaFixture>
 {
     [Fact]
+    public void ThrowsIfTxnIDIsNotExclusive()
+    {
+        // Arrange
+        var rq = new SalesOrderQuery() { TxnID = ["ABC"] };
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumber = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberCaseSensitive = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.MaxReturned = 1);
+        Assert.Throws<InvalidOperationException>(() => rq.ModifiedDateRangeFilter = new() { FromModifiedDate = DateTime.Now });
+        Assert.Throws<InvalidOperationException>(() => rq.TxnDateRangeFilter = new() { DateMacro = DateMacro.All });
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberFilter = RefNumberFilter.Contains("ABC"));
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberRangeFilter = new RefNumberRangeFilter() { FromRefNumber = "ABC" });
+        Assert.Throws<InvalidOperationException>(() => rq.EntityFilter = new() { FullName = ["ABC"] });
+    }
+
+    [Fact]
+    public void ThrowsIfRefNumberIsNotExclusive()
+    {
+        // Arrange
+        var rq = new SalesOrderQuery() { RefNumber = ["ABC"] };
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(() => rq.TxnID = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberCaseSensitive = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.MaxReturned = 1);
+        Assert.Throws<InvalidOperationException>(() => rq.ModifiedDateRangeFilter = new() { FromModifiedDate = DateTime.Now });
+        Assert.Throws<InvalidOperationException>(() => rq.TxnDateRangeFilter = new() { DateMacro = DateMacro.All });
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberFilter = RefNumberFilter.Contains("ABC"));
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberRangeFilter = new RefNumberRangeFilter() { FromRefNumber = "ABC" });
+        Assert.Throws<InvalidOperationException>(() => rq.EntityFilter = new() { FullName = ["ABC"] });
+    }
+
+    [Fact]
+    public void ThrowsIfRefNumberCaseSensitiveIsNotExclusive()
+    {
+        // Arrange
+        var rq = new SalesOrderQuery() { RefNumberCaseSensitive = ["ABC"] };
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(() => rq.TxnID = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumber = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.MaxReturned = 1);
+        Assert.Throws<InvalidOperationException>(() => rq.ModifiedDateRangeFilter = new() { FromModifiedDate = DateTime.Now });
+        Assert.Throws<InvalidOperationException>(() => rq.TxnDateRangeFilter = new() { DateMacro = DateMacro.All });
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberFilter = RefNumberFilter.Contains("ABC"));
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberRangeFilter = new RefNumberRangeFilter() { FromRefNumber = "ABC" });
+        Assert.Throws<InvalidOperationException>(() => rq.EntityFilter = new() { FullName = ["ABC"] });
+    }
+
+    [Fact]
+    public void ThrowsIfMaxReturnedIsNotExclusive()
+    {
+        // Arrange
+        var rq = new SalesOrderQuery() { MaxReturned = 1 };
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(() => rq.TxnID = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberCaseSensitive = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumber = ["1"]);
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberFilter = RefNumberFilter.Contains("ABC"));
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberRangeFilter = new RefNumberRangeFilter() { FromRefNumber = "ABC" });
+    }
+
+    [Fact]
+    public void ThrowsIfModifiedDateRangeFilterIsNotExclusive()
+    {
+        // Arrange
+        var rq = new SalesOrderQuery() { ModifiedDateRangeFilter = new() { FromModifiedDate = DateTime.Now } };
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(() => rq.TxnID = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberCaseSensitive = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumber = ["1"]);
+        Assert.Throws<InvalidOperationException>(() => rq.TxnDateRangeFilter = new() { DateMacro = DateMacro.All });
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberFilter = RefNumberFilter.Contains("ABC"));
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberRangeFilter = new RefNumberRangeFilter() { FromRefNumber = "ABC" });
+    }
+
+    [Fact]
+    public void ThrowsIfTxnDateRangeFilterIsNotExclusive()
+    {
+        // Arrange
+        var rq = new SalesOrderQuery() { TxnDateRangeFilter = new() { DateMacro = DateMacro.All } };
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(() => rq.TxnID = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberCaseSensitive = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumber = ["1"]);
+        Assert.Throws<InvalidOperationException>(() => rq.ModifiedDateRangeFilter = new() { FromModifiedDate = DateTime.Now });
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberFilter = RefNumberFilter.Contains("ABC"));
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberRangeFilter = new RefNumberRangeFilter() { FromRefNumber = "ABC" });
+    }
+
+    [Fact]
+    public void ThrowsIfRefNumberFilterIsNotExclusive()
+    {
+        // Arrange
+        var rq = new SalesOrderQuery() { RefNumberFilter = RefNumberFilter.Contains("ABC") };
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(() => rq.TxnID = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberCaseSensitive = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.MaxReturned = 1);
+        Assert.Throws<InvalidOperationException>(() => rq.ModifiedDateRangeFilter = new() { FromModifiedDate = DateTime.Now });
+        Assert.Throws<InvalidOperationException>(() => rq.TxnDateRangeFilter = new() { DateMacro = DateMacro.All });
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumber = ["ABC"]);
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberRangeFilter = new RefNumberRangeFilter() { FromRefNumber = "ABC" });
+    }
+
+    [Fact]
+    public void ThrowsIfRefNumberRangeFilterIsNotExclusive()
+    {
+        // Arrange
+        var rq = new SalesOrderQuery() { RefNumberRangeFilter = new() { FromRefNumber = "ABC" } };
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(() => rq.TxnID = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberCaseSensitive = ["123"]);
+        Assert.Throws<InvalidOperationException>(() => rq.MaxReturned = 1);
+        Assert.Throws<InvalidOperationException>(() => rq.ModifiedDateRangeFilter = new() { FromModifiedDate = DateTime.Now });
+        Assert.Throws<InvalidOperationException>(() => rq.TxnDateRangeFilter = new() { DateMacro = DateMacro.All });
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumber = ["ABC"]);
+        Assert.Throws<InvalidOperationException>(() => rq.RefNumberFilter = RefNumberFilter.Contains("ABC"));
+    }
+
+    [Fact]
     public void GeneratesCorrectRequestString()
     {
         // Arrange
         var rq = new SalesOrderQuery()
         {
-            RefNumber = ["12345"],
+            MaxReturned = 100,
+            TxnDateRangeFilter = new() { DateMacro = DateMacro.All },
+            EntityFilter = new() { FullName = ["Customer"] },
+            CurrencyFilter = new() { FullName = ["USD"] },
+            IncludeLineItems = true,
+            IncludeLinkedTxns = true,
+            IncludeRetElement = ["TxnDate", "RefNumber", "CustomerRef"],
+            OwnerID = ["0"]
         };
 
         var qbxml = new QBXMLRequest([rq]);
