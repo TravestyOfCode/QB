91422698a63bfabb08d1848a398ad02ecc25c47e
travis@travestyofcode.com <travis@travestyofcode.com>
2026-01-04 16:07:38 -0500

    Moved MaxReturned from base class as it is an exclusive property in queries.

--- QB.SDK/Requests/Query/QueryRq.cs
+++ QB.SDK/Requests/Query/QueryRq.cs
@@ -7,30 +7,4 @@ public abstract class QueryRq : QBRequest
     public string? iteratorID { get; set; }
 
     public MetaData? metaData { get; set; }
-
-    private int? _MaxReturned;
-    public int? MaxReturned
-    {
-        get => _MaxReturned;
-        set
-        {
-            if (string.IsNullOrWhiteSpace(iteratorID))
-            {
-                if (value != null)
-                {
-                    iterator = Iterator.Start;
-                }
-                else
-                {
-                    iterator = null;
-                }
-            }
-            else
-            {
-                iterator = Iterator.Continue;
-            }
-
-            _MaxReturned = value;
-        }
-    }
 }
