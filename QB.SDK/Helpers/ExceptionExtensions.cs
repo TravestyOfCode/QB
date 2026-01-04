3df115f2bb831e7951ce17a54c91c88b8a691246
travis@travestyofcode.com <travis@travestyofcode.com>
2026-01-04 16:07:03 -0500

    Added Exceptions for QBSDK errors and helper for exclusive properties.

---
+++ QB.SDK/Helpers/ExceptionExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace QB.SDK;
+
+public static class ExceptionExtensions
+{
+    public static void ExclusiveThrow<T>(this T? value, string property, [CallerArgumentExpression(nameof(value))] string source = "")
+    {
+        if (value != null)
+        {
+            throw new InvalidOperationException($"Unable to set value to {property} while {source} has a non-null value.");
+        }
+    }
+}
